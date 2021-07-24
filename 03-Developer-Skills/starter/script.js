58e4ec7d3b08330f8cbd5cbac0afff00f679b046
Simon Taylor <simon.taylor@tyxan.com>
2021-07-24 10:00:32 +0100

    Completed section 3

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,20 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/**
+ * ==========================
+ * DEBUGGING
+ * ==========================
+ */
+const x = 23;
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => {
+  //debugger; // Stops console at this point to inspect operation and further operations
+  return [2037 - birthYear, 'another var', 'one more var'];
+};
+
+console.log(calcAge(1991)); // Regular log
+console.warn(calcAge(1991)); // Log as warning (orange)
+console.error(calcAge(1991)); // Log as error (red)
+console.table(calcAge(1991)); // Log as table
